2309987b5160e74469bcce008a62c2d5ae6a6380
Dan Pike <danny.pike@gmail.com>
2020-05-27 15:37:26 +0100

    added a few basic Mocking tests for IHtmlBody

--- Tests/Helpers.cs
+++ Tests/Helpers.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Text;
 
 namespace Tests
@@ -17,6 +18,12 @@ namespace Tests
         public const string ThreePagesTxt = @"..\..\..\..\samples\threePages.txt";
         public const string ExistingPdf = @"..\..\..\..\samples\existing.pdf";
         public const string SampleThreePagesPdf = @"..\..\..\..\Tests\outputs\sampleThreePages.pdf";
+        public const string SingleTextPdf = @"..\..\..\..\Tests\outputs\singleText.pdf";
+        public const string SingleTextTxt = @"..\..\..\..\Tests\inputs\singleText.txt";
+        public const string MultipleTextPdf = @"..\..\..\..\Tests\outputs\multipleText.pdf";
+        public const string MultipleTextTxt = @"..\..\..\..\Tests\inputs\multipleText.txt";
+        public const string LargeAndNormalPdf = @"..\..\..\..\Tests\outputs\largeAndNormalText.pdf";
+        public const string LargeAndNormalTxt = @"..\..\..\..\Tests\inputs\largeAndNormalText.txt";
 
         /// <summary>
         /// Delete the outFile, if it exists and check that it was deleted
@@ -24,14 +31,72 @@ namespace Tests
         /// </summary>
         /// <param name="outFile">Delete this file, if it exists</param>
         /// <param name="inFile">(optional) Check to see that this file exists</param>
-        public static void ArrangeFiles(string outFile, string inFile = null)
+        /// <param name="deleteInFile">(optional) Also delete the input file, if
+        /// it exists; inFile must be non-null if deleteInFile is true</param>
+        public static void ArrangeFiles(string outFile, string inFile = null, bool deleteInFile = false)
         {
             if (File.Exists(outFile))
             {
                 File.Delete(outFile);
             }
-            Assert.AreEqual(true, inFile != null ? File.Exists(inFile) : true);
+            if (deleteInFile)
+            {
+                if (File.Exists(inFile))
+                {
+                    File.Delete(inFile);
+                }
+                Assert.AreEqual(false, File.Exists(inFile));
+            }
+            else
+            {
+                Assert.AreEqual(true, inFile != null ? File.Exists(inFile) : true);
+            }
             Assert.AreEqual(false, File.Exists(outFile));
+        }
+
+        /// <summary>
+        /// <see cref="BuildInFile(string, IEnumerable{string})"/>
+        /// </summary>
+        public static void BuildFile(string inFile, params string[] lines)
+        {
+            BuildInFile(inFile, lines.ToList());
+        }
+
+        /// <summary>
+        /// Build a file from a list of strings, one line-per-string
+        /// </summary>
+        /// <param name="inFile">The name of the input file. Directory will be created
+        /// if it does not already exist</param>
+        /// <param name="lines">The lines to add to the file</param>
+        public static void BuildInFile(string inFile, IEnumerable<string> lines)
+        {
+            inFile = Path.GetFullPath(inFile);
+            string directory = Path.GetDirectoryName(inFile);
+            if (!Directory.Exists(directory))
+            {
+                Directory.CreateDirectory(directory);
+                Assert.AreEqual(true, Directory.Exists(directory),
+                    $"failed to create directory '{directory}'");
+            }
+
+            // Make sure that we are not using an old file
+            if (File.Exists(inFile))
+            {
+                File.Delete(inFile);
+            }
+            Assert.AreEqual(false, File.Exists(inFile));
+
+            var sb = new StringBuilder();
+            sb.AppendJoin("\n", lines);
+            if (sb.Length > 0)
+            {
+                // The final line will not have had a separator ("\n") joined to the end
+                sb.Append('\n');
+            }
+            File.WriteAllText(inFile, sb.ToString());
+
+            // It should have thrown an exception if we failed to create the file but it doesn't hurt to verify that
+            Assert.AreEqual(true, File.Exists(inFile), $"failed to build input file {inFile}");
         }
     }
 }
--- Tests/TestHtmlBody.cs
+++ Tests/TestHtmlBody.cs
@@ -1,70 +1,167 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
 using Moq;
 using NUnit.Framework;
 using PdfBuilder;
 using PdfBuilder.Abstractions;
 using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace Tests
 {
     public class TestHtmlBody
     {
+        public delegate void MockSetup(Mock<IHtmlBody> mockBody);
+
+        /// <summary>
+        /// An IHtmlBodyFactory implementation that uses a singleton IHtmlBody for us to setup with Moq
+        /// </summary>
         class Factory : IHtmlBodyFactory
         {
-            public Mock<IHtmlBody> Mocker { get; set; }
-
-            public IHtmlBody Create(IServiceProvider DI)
+            public Factory(MockSetup mockSetup)
             {
-                Mocker = new Mock<IHtmlBody>();
-                return Mocker.Object;
+                mock_ = new Mock<IHtmlBody>();
+                mockSetup(mock_);
             }
+            public IHtmlBody Create(IServiceProvider DI) => mock_.Object;
+
+            private Mock<IHtmlBody> mock_ { get; set; }
         }
 
-        [SetUp]
-        public void Setup()
+        /// <summary>
+        /// A helper for setting up a Mock&lt;IHtmlBody&gt; that will be returned by
+        /// the IServiceProvider logic inside PdfBuilder.Builder.
+        /// </summary>
+        /// <param name="setup">A setup function that is invoked when the IHtmlBodyFactory
+        /// constructs the mock IHtmlBody</param>
+        /// <returns>Returns the PdfBuilder.Builder to use in the test</returns>
+        public static Builder MockBuilder(MockSetup setup)
         {
-            // Set up a DI provider that allows us to mock the IHtmlBody
-            var services = new ServiceCollection()
-                .AddLogging(builder => builder
-                    .SetMinimumLevel(LogLevel.Trace)
-                    .AddConsole(options => options.LogToStandardErrorThreshold = LogLevel.Trace)
-                    );
-            services.Add(new ServiceDescriptor(typeof(IHtmlBodyFactory), (services) =>
-            {
-                // Export the IHtmlBodyFactory to the Test class, so that we can mock the IHtmlBody
-                return factory_ = new Factory();
-            }, ServiceLifetime.Singleton));
+            // Because we have a new IServiceProvider for each test, we can capture the Factory instance
+            // and setup the Mock<IHtmlBody> that the test will use.
+            var services = new ServiceCollection();
+            services.AddTransient<IHtmlBodyFactory>(ss => new Factory(setup));
 
-            // And construct a Builder from it
-            pdfBuilder_ = new Builder();
-            pdfBuilder_.DI = services.BuildServiceProvider();
+            // Construct and return a Builder that will use this injector (as an [out] parameter)
+            return new Builder { DI = services.BuildServiceProvider() };
         }
 
-        [TestCase(Helpers.Sample1Pdf, Helpers.Sample1Txt, TestName = "Verify that the DI creates a Mockable IHtmlBody")]
-        public async Task CreatesMockableHtmlBody(string outFile, string inFile)
+        [TestCase(Helpers.SingleTextPdf, Helpers.SingleTextTxt, TestName = "Single line of text only")]
+        public async Task SingleLineTextOnly(string outFile, string inFile)
         {
-            Helpers.ArrangeFiles(outFile, inFile);
+            Helpers.ArrangeFiles(outFile, inFile, true);
+            Helpers.BuildFile(inFile, "Hello, world");
 
-            Assert.IsNotNull(pdfBuilder_);
-
-            // PdfBuilder.Create() will throw an exception, because the Mock<> version has no logic
-            // We're not interested in that exception ...
-            try
+            var pdfBuilder = MockBuilder(mocker =>
             {
-                await pdfBuilder_.Create(outFile, inFile);
-            }
-            catch { }
+                var seq = new MockSequence();
 
-            // ... but we do want to verify that a Mocker was cerated, i.e. the Create() call used our DI system
-            Assert.IsNotNull(factory_.Mocker, "DI failed to mock the IHtmlBodyFactory");
+                // Builder.Create() will call AddText() ...
+                mocker.InSequence(seq)
+                    .Setup(bb => bb.AddText("Hello, world"))
+                    .Returns(PdfErrors.Success);
+
+                // .. and then it will call IHtmlBody.Render() ...
+                mocker.InSequence(seq)
+                    .Setup(bb => bb.Render())
+                    .Returns(PdfErrors.Success)
+                    ;
+
+                // ... and then it will retrieve the rendered HTML
+                mocker.InSequence(seq)
+                    .Setup(bb => bb.RenderedHtml)
+                    .Returns("<body>Hello, world</body>");
+            });
+
+            Assert.AreEqual(PdfErrors.Success, await pdfBuilder.Create(outFile, inFile));
         }
 
-        private Factory factory_;
-        private Builder pdfBuilder_;
+        [TestCase(Helpers.MultipleTextPdf, Helpers.MultipleTextTxt, TestName = "Multiple lines of text")]
+        public async Task MultipleLinesTextOnly(string outFile, string inFile)
+        {
+            Helpers.ArrangeFiles(outFile, inFile, true);
+            Helpers.BuildFile(inFile, @"
+Hello,
+world
+");
+            var pdfBuilder = MockBuilder(mocker =>
+            {
+                var seq = new MockSequence();
+
+                mocker.InSequence(seq)
+                    .Setup(bb => bb.AddText("Hello,"))
+                    .Returns(PdfErrors.Success);
+
+                mocker.InSequence(seq)
+                    .Setup(bb => bb.AddText(" "))
+                    .Returns(PdfErrors.Success);
+
+                mocker.InSequence(seq)
+                    .Setup(bb => bb.AddText("world"))
+                    .Returns(PdfErrors.Success);
+
+                mocker.InSequence(seq)
+                    .Setup(bb => bb.Render())
+                    .Returns(PdfErrors.Success)
+                    ;
+
+                mocker.InSequence(seq)
+                    .Setup(bb => bb.RenderedHtml)
+                    .Returns("<body>Hello, world</body>");
+            });
+
+            Assert.AreEqual(PdfErrors.Success, await pdfBuilder.Create(outFile, inFile));
+        }
+
+        [TestCase(Helpers.LargeAndNormalPdf, Helpers.LargeAndNormalTxt, TestName = "Large and normal commands")]
+        public async Task LargeAndSmallText(string outFile, string inFile)
+        {
+            Helpers.ArrangeFiles(outFile, inFile, true);
+            Helpers.BuildFile(inFile, @"
+.large
+Hello!
+.normal
+Hi, world
+");
+
+            int sequence = 0;
+            var pdfBuilder = MockBuilder(mocker =>
+            {
+                mocker
+                    .Setup(bb => bb.StartHeading())
+                    .Callback(() => { Assert.AreEqual(1, ++sequence); })
+                    .Returns(PdfErrors.Success);
+
+                mocker
+                    .Setup(bb => bb.AddText("Hello!"))
+                    .Callback(() => { Assert.AreEqual(2, ++sequence); })
+                    .Returns(PdfErrors.Success);
+
+                mocker
+                    .Setup(bb => bb.StartBody())
+                    .Callback(() => { Assert.AreEqual(3, ++sequence); })
+                    .Returns(PdfErrors.Success);
+
+                mocker
+                    .Setup(bb => bb.AddText("Hi, world"))
+                    .Callback(() => { Assert.AreEqual(4, ++sequence); })
+                    .Returns(PdfErrors.Success);
+
+                mocker
+                    .Setup(bb => bb.Render())
+                    .Callback(() => { Assert.AreEqual(5, ++sequence); })
+                    .Returns(PdfErrors.Success)
+                    ;
+
+                mocker
+                    .Setup(bb => bb.RenderedHtml)
+                    .Callback(() => { Assert.AreEqual(6, ++sequence); })
+                    .Returns("<body>Hello, world</body>");
+            });
+
+            var result = await pdfBuilder.Create(outFile, inFile);
+
+            Assert.AreEqual(PdfErrors.Success, result);
+            Assert.AreEqual(6, sequence, "Not all methods were invoked");
+        }
     }
 }
